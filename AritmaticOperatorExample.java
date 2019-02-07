ba8ec34ebf75b04b192f3ef93c216324ac07b77e
unknown <faiz.mujtaba@yahoo.com>
2019-02-07 11:32:01 +0530

    added java code for arithmatic operation

---
+++ AritmaticOperatorExample.java
@@ -0,0 +1,11 @@
+package com.sonata;
+
+public class AritmaticOperatorExample {
+	public static void main(String[] args) {
+		int a=20,b=3;
+		System.out.println("value of a = "+a);
+		System.out.println("value of b = "+b);
+		System.out.println("value of a mod b = "+a%b);
+	}
+
+}
